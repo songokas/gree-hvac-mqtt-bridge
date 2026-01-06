dac489d1ab1de729333ba0648a18f2a6f928fbe2
Tomas Jakstas <tom.jakstas@gmail.com>
2026-01-06 20:19:24 +0200

    Fix audit and modify script to only depend on node

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 
-CONFIG_PATH=/data/options.json
+CONFIG_PATH="${1:-"data/options.json"}"
 
 HVAC_HOST=$(jq -r ".hvac_host" $CONFIG_PATH)
 MQTT_BROKER_URL=$(jq -r ".mqtt.broker_url" $CONFIG_PATH)
@@ -14,7 +15,6 @@ if [ "$MQTT_RETAIN" = null ]; then
 fi
 
 echo "MQTT_RETAIN: ${MQTT_RETAIN}"
-npm install
 
 INSTANCES=$(jq '.devices | length' $CONFIG_PATH)
 
@@ -25,15 +25,14 @@ if [ "$INSTANCES" -gt 1 ]; then
 		if [[ $HVAC_HOST = null ]]; then echo "[ERROR] Missing hvac_host for device $i. Skipping." && continue; fi
 		if [[ $MQTT_TOPIC_PREFIX = null ]]; then echo "[ERROR] Missing mqtt_topic_prefix for device $i. Skipping." && continue; fi
 		echo "Running instance $i for $HVAC_HOST"
-		npx pm2 start index.js --silent -m --merge-logs --name="HVAC_${i}" -- \
+		/usr/bin/node index.js \
 			--hvac-host="${HVAC_HOST}" \
 			--mqtt-broker-url="${MQTT_BROKER_URL}" \
 			--mqtt-topic-prefix="${MQTT_TOPIC_PREFIX}" \
 			--mqtt-username="${MQTT_USERNAME}" \
 			--mqtt-password="${MQTT_PASSWORD}" \
-			--mqtt-retain="${MQTT_RETAIN}"
+			--mqtt-retain="${MQTT_RETAIN}" &
 	done
-	npx pm2 logs /HVAC_/
 else
 	HVAC_HOST=$(jq -r ".devices[0].hvac_host" $CONFIG_PATH);
 	MQTT_TOPIC_PREFIX=$(jq -r ".devices[0].mqtt_topic_prefix" $CONFIG_PATH);
@@ -47,3 +46,5 @@ else
 		--mqtt-password="${MQTT_PASSWORD}" \
 		--mqtt-retain="${MQTT_RETAIN}"
 fi
+
+wait
